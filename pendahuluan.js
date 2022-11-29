702aa78bc577e99f6f18f82809f399eabf6beaea
agungsetyonugrohoo <agungsn94@gmail.com>
2022-11-29 15:45:48 +0700

    pendahuluan mengenai penggunaan javascript pada DOM

---
+++ pendahuluan.js
@@ -0,0 +1,36 @@
+/* 
+DOM (Document Object Model) => 
+- Antarmuka pemrograman berbasis objek yang merepresentasikan dokumen web
+- DOM membuat seluruh komponen dari halaman web dapat diakses & dimanipulasi
+- Komponen 
+    - Elemen HTML
+    - Atribut
+    - Text
+    - dll
+- DOM dapat dimanipulasi (dibuat, diubah, dihapus) menggunakan javascript
+
+Definisi :
+- Mozilla => DOM adalah antarmuka pemrograman untuk HTML yang merepresentasikan halaman web, sehingga program dapat mengubah dan memanipulasi strukturnya
+- Sandhika Galih => Representasi elemen HTML pada dokumen menjadi sebuah object
+
+Pohon Hirarki DOM (DOM Tree) => Struktur dasar hirarki pembentuk sebuah halaman web yang dimana seluruhnya dibungkus didalam suatu object yaitu document
+
+Materi Javascript & DOM :
+- DOM Selection
+- DOM Manipulation
+- DOM Traversal
+- Event Handling
+- Studi Kasus
+    - membuat program2 sederhana
+
+Pre-requisite :
+- HTML
+- CSS
+- Javascript
+
+Aplikasi pendukung :
+- Code Editor
+- Web Browser
+*/
+
+console.log(document);
